67907654e6efff6955cb0db9e150008a9c30cf62
marianedealmeida5609 <marianedealmeida5609@gmail.com>
2025-04-05 22:09:28 +0000

    criação do javascrips e das funções da div info

---
+++ main.js
@@ -0,0 +1,11 @@
+let bill = 0
+let tipPercentage = 0
+let numberOfPeople
+
+function receiveBillValue() {
+    bill = document.querySelector("#bill").valueAsNumber
+}
+
+function receiveNumberOfPeopleValue () {
+    numberOfPeople = document.querySelector("#people").valueAsNumber
+}
